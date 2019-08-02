5a56e7c028b76b961273fae825ff9decfca19e2a
OptimShi <ross@yotesfan.com>
2019-08-02 13:07:25 -0700

    Search for all uses of Armor Layering Kits

--- aclogview/Tools/FindOpcodeInFilesForm.cs
+++ aclogview/Tools/FindOpcodeInFilesForm.cs
@@ -100,29 +100,35 @@ namespace aclogview
 
         // key is [Name of Container] + "," + WCID + "," + [LootName]
         // val is the number of hits
-        private Dictionary<string, uint> Creatures = new Dictionary<string, uint>();
+        private List<string> LogsFound = new List<string>();
+        private uint ResultsFound = 0;
 
         DateTime dt = DateTime.Now;
 
-        private string logFileName { get { return "D:\\Source\\DamageRatingsLandblock-" + dt.ToString("yyyy-MM-dd") + ".csv"; } }
+        private string logFileName { get { return "D:\\Source\\ArmorLayeringUse-" + dt.ToString("yyyy-MM-dd") + ".csv"; } }
 
         private void ResetLogFile()
         {
             using (StreamWriter theFile = new StreamWriter(logFileName, false))
             {
-                theFile.WriteLine("wcid,name,damage rating,landblock,hits");
+                theFile.WriteLine("wcid,name,pcap");
             }
         }
 
         private void SaveResultsToLogFile()
         {
-            if (Creatures.Count == 0) return;
+            if (LogsFound.Count == 0) return;
 
             using (StreamWriter theFile = new StreamWriter(logFileName, true))
             {
-                foreach (var e in Creatures)
-                    theFile.WriteLine(e.Key + "," + e.Value);
+                foreach (var e in LogsFound)
+                {
+                    theFile.WriteLine(e);
+                    ResultsFound++;
+                }
             }
+
+            LogsFound.Clear();
         }
 
         private void btnStartSearch_Click(object sender, EventArgs e)
@@ -219,21 +225,26 @@ namespace aclogview
                 }
                 catch { }
             }
-
-            SaveResultsToLogFile();
+            LogProgress(progress, filesToProcess.Count, "COMPLETE");
         }
 
         private void LogProgress(int progress, int total, string filename)
         {
+            SaveResultsToLogFile();
+
             using (StreamWriter theFile = new StreamWriter("D:\\Source\\aclogview_progress.txt", false))
             {
                 //Calculate percentage earlier in code
                 decimal percentage = (decimal)progress / total;
                 theFile.WriteLine(progress.ToString() + " of " + total.ToString() + " - " + percentage.ToString("0.00%"));
                 theFile.WriteLine(filename);
-                theFile.WriteLine("Creature Entries: " + Creatures.Count.ToString());
+                theFile.WriteLine("Results Found: " + ResultsFound.ToString());
             }
         }
+
+        public const uint ARMOR_MAIN_REDUCTION_TOOL = 42622;
+        public const uint ARMOR_LOWER_REDUCTION_TOOL = 44879;
+        public const uint ARMOR_MIDDLE_REDUCTION_TOOL = 44880;
 
         private void ProcessFile(string fileName)
         {
@@ -273,6 +284,10 @@ namespace aclogview
                     // Store all the created weenies!
                     switch (opcode)
                     {
+                        case PacketOpcode.CHARACTER_ENTER_GAME_EVENT:
+                            // Reset the list when a player logs in
+                            CreateObjectList.Clear();
+                            break;
                         case PacketOpcode.Evt_Physics__DeleteObject_ID:
                             var delMessage = CM_Physics.DeleteObject.read(messageDataReader);
                             uint delObjectId = delMessage.object_id;
@@ -286,8 +301,6 @@ namespace aclogview
                             uint objectId = message.object_id;
 
                             // Check if the item is a creature and NOT a player
-                            if(message.wdesc._type == ITEM_TYPE.TYPE_CREATURE && message.wdesc._wcid != 1)
-                            {
                                 if (CreateObjectList.ContainsKey(objectId))
                                 {
                                     CreateObjectList[objectId] = message;
@@ -296,41 +309,22 @@ namespace aclogview
                                 {
                                     CreateObjectList.Add(objectId, message);
                                 }
-                            }
                             break;
-                        case PacketOpcode.APPRAISAL_INFO_EVENT:
-                            var appraisalMessage = CM_Examine.SetAppraiseInfo.read(messageDataReader);
-                            uint appraisalID = appraisalMessage.i_objid;
-                            if (appraisalMessage.i_prof.success_flag == 1 && CreateObjectList.ContainsKey(appraisalID))
-                            {
-                                string result = CreateObjectList[appraisalID].wdesc._wcid + ",\"" + CreateObjectList[appraisalID].wdesc._name + "\",";
-                                uint wcid = CreateObjectList[appraisalID].wdesc._wcid;
-                                int damageRating = 0;
-                                if (appraisalMessage.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.DAMAGE_RATING_INT))
-                                {
-                                    damageRating = appraisalMessage.i_prof._intStatsTable.hashTable[STypeInt.DAMAGE_RATING_INT];
-                                    result += appraisalMessage.i_prof._intStatsTable.hashTable[STypeInt.DAMAGE_RATING_INT];
-                                }
-                                /*
-                                if(wcid == 194 && damageRating == 5)
-                                {
-                                    var alertMessage = fileName + "\r\n" + "Line " + packetCount.ToString() + "\r\n";
-                                    richTextBox1.AppendText(alertMessage);
-                                    MessageBox.Show(alertMessage);
-                                }
-                                */
-                                result += "," + CreateObjectList[appraisalID].physicsdesc.pos.objcell_id.ToString("X8");
+                        case PacketOpcode.Evt_Inventory__UseWithTargetEvent_ID:
+                            var useMessage = CM_Inventory.UseWithTargetEvent.read(messageDataReader);
 
-                                if (Creatures.ContainsKey(result))
+                            var sourceGUID = useMessage.i_object;
+                            if (CreateObjectList.ContainsKey(sourceGUID))
+                            {
+                                var sourceWCID = CreateObjectList[sourceGUID].wdesc._wcid;
+                                if(sourceWCID == ARMOR_MAIN_REDUCTION_TOOL || sourceWCID == ARMOR_LOWER_REDUCTION_TOOL || sourceWCID == ARMOR_MIDDLE_REDUCTION_TOOL)
                                 {
-                                    Creatures[result]++;
+                                    string sourceName = CreateObjectList[sourceGUID].wdesc._name.m_buffer;
+                                    string result = $"{sourceWCID},{sourceName},{fileName}";
+                                    LogsFound.Add(result);
                                 }
-                                else
-                                {
-                                    Creatures[result] = 1;
-                                }
-                                //results.Add(result);
                             }
+
                             break;
                     }
 
@@ -356,210 +350,6 @@ namespace aclogview
 
 
             //processFileResults.Add(new ProcessFileResult() { FileName = fileName, Hits = hits, Exceptions = exceptions });
-        }
-
-        private string AddToResults(CM_Inventory.ViewContents contents, Dictionary<uint, CM_Physics.CreateObject> CreateObjectList, Dictionary<uint, CM_Examine.SetAppraiseInfo> AppraisalList)
-        {
-            string result = "";
-            var containerId = contents.i_container;
-            // We know what this container is...
-            if (CreateObjectList.ContainsKey(containerId))
-            {
-                var newObj = CreateObjectList[containerId];
-                // Make sure the container doesn't have a container id (e.g. it's a Pack in a player's inventory...)
-                if ((newObj.wdesc.header & (uint)PublicWeenieDescPackHeader.PWD_Packed_ContainerID) == 0)
-                {
-                    string containerName = newObj.wdesc._name.m_buffer;
-                    containerName = containerName.Replace("Corpse of ", "");
-                    containerName = containerName.Replace("Treasure of ", "");
-
-                    string containerPos = "";
-                    if((newObj.physicsdesc.bitfield & (uint)0x8000) != 0)
-                        containerPos = newObj.physicsdesc.pos.objcell_id.ToString("X8");
-
-                    //uint containerWCID = GetParentWeenieFromCorpse(newObj, CreateObjectList, Positions);
-                    string containerWCID;
-                    if (newObj.wdesc._wcid == 21) // Remove the "Corpse" weenies
-                        containerWCID = "";
-                    else
-                        containerWCID = newObj.wdesc._wcid.ToString();
-
-                    // Cycle through all the contents of the container
-                    for (int i = 0; i < contents.contents_list.list.Count; i++)
-                    {
-                        var thisContent = contents.contents_list.list[i];
-                        uint thisContentGUID = thisContent.m_iid;
-
-                        if (CreateObjectList.ContainsKey(thisContentGUID) && AppraisalList.ContainsKey(thisContentGUID))
-                        {
-                            var co = CreateObjectList[thisContentGUID];
-                            var app = AppraisalList[thisContentGUID];
-
-                            string lootName = co.wdesc._name.m_buffer;
-                            uint lootWCID = co.wdesc._wcid;
-                            uint value = co.wdesc._value;
-                            uint materialId = (uint)co.wdesc._material_type;
-
-                            string itemLevel = "";
-                            if (co.wdesc._iconOverlayID > 0)
-                            {
-                                switch (co.wdesc._iconOverlayID)
-                                {
-                                    case 0x06006C34:
-                                        itemLevel = "1";
-                                        break;
-                                    case 0x06006c35:
-                                        itemLevel = "2";
-                                        break;
-                                    case 0x06006c36:
-                                        itemLevel = "3";
-                                        break;
-                                    case 0x06006c37:
-                                        itemLevel = "4";
-                                        break;
-                                    case 0x06006c38:
-                                        itemLevel = "5";
-                                        break;
-                                }
-                            }
-
-                            if (materialId > 0 || itemLevel != "")
-                            {
-                                string workmanship = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.ITEM_WORKMANSHIP_INT))
-                                    workmanship = app.i_prof._intStatsTable.hashTable[STypeInt.ITEM_WORKMANSHIP_INT].ToString();
-
-                                string numTinks = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.NUM_TIMES_TINKERED_INT))
-                                    numTinks = app.i_prof._intStatsTable.hashTable[STypeInt.NUM_TIMES_TINKERED_INT].ToString();
-
-                                string gemCount = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.GEM_COUNT_INT))
-                                    gemCount = app.i_prof._intStatsTable.hashTable[STypeInt.GEM_COUNT_INT].ToString();
-
-                                string gemMaterial = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.GEM_TYPE_INT))
-                                    gemMaterial = app.i_prof._intStatsTable.hashTable[STypeInt.GEM_TYPE_INT].ToString();
-
-                                string spells = "";
-                                for (var j = 0; j < app.i_prof._spellsTable.list.Count; j++)
-                                    spells += app.i_prof._spellsTable.list[j].ToString() + "|";
-
-                                string itemType = co.wdesc._type.ToString();
-
-                                string clothingPriority = "";
-                                if (co.wdesc._priority > 0)
-                                    clothingPriority = co.wdesc._priority.ToString();
-                                string locations = "";
-                                if (co.wdesc._valid_locations > 0)
-                                    locations = co.wdesc._valid_locations.ToString();
-
-                                string wieldReq = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.WIELD_REQUIREMENTS_INT))
-                                    wieldReq = app.i_prof._intStatsTable.hashTable[STypeInt.WIELD_REQUIREMENTS_INT].ToString();
-                                string wieldSkillType = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.WIELD_SKILLTYPE_INT))
-                                    wieldSkillType = app.i_prof._intStatsTable.hashTable[STypeInt.WIELD_SKILLTYPE_INT].ToString();
-                                string wieldDiff = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.WIELD_DIFFICULTY_INT))
-                                    wieldDiff = app.i_prof._intStatsTable.hashTable[STypeInt.WIELD_DIFFICULTY_INT].ToString();
-
-                                string wieldReq2 = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.WIELD_REQUIREMENTS_2_INT))
-                                    wieldReq2 = app.i_prof._intStatsTable.hashTable[STypeInt.WIELD_REQUIREMENTS_2_INT].ToString();
-                                string wieldSkillType2 = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.WIELD_SKILLTYPE_2_INT))
-                                    wieldSkillType2 = app.i_prof._intStatsTable.hashTable[STypeInt.WIELD_SKILLTYPE_2_INT].ToString();
-                                string wieldDiff2 = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.WIELD_DIFFICULTY_2_INT))
-                                    wieldDiff2 = app.i_prof._intStatsTable.hashTable[STypeInt.WIELD_DIFFICULTY_2_INT].ToString();
-
-                                string weaponType = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.WEAPON_TYPE_INT))
-                                    weaponType = app.i_prof._intStatsTable.hashTable[STypeInt.WEAPON_TYPE_INT].ToString();
-
-                                string spellSet = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.EQUIPMENT_SET_ID_INT))
-                                    spellSet = app.i_prof._intStatsTable.hashTable[STypeInt.EQUIPMENT_SET_ID_INT].ToString();
-
-                                string description = "";
-                                if (app.i_prof._strStatsTable.hashTable.ContainsKey(STypeString.LONG_DESC_STRING))
-                                {
-                                    description = app.i_prof._strStatsTable.hashTable[STypeString.LONG_DESC_STRING].m_buffer;
-                                    description = description.Replace("\"", "\"\""); // escape quotes with...another quote? CSV is weird.
-                                }
-
-                                string difficulty = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.ITEM_DIFFICULTY_INT))
-                                    difficulty = app.i_prof._intStatsTable.hashTable[STypeInt.ITEM_DIFFICULTY_INT].ToString();
-
-                                string spellcraft = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.ITEM_SPELLCRAFT_INT))
-                                    spellcraft = app.i_prof._intStatsTable.hashTable[STypeInt.ITEM_SPELLCRAFT_INT].ToString();
-
-                                string maxMana = "";
-                                if (app.i_prof._intStatsTable.hashTable.ContainsKey(STypeInt.ITEM_MAX_MANA_INT))
-                                    maxMana = app.i_prof._intStatsTable.hashTable[STypeInt.ITEM_MAX_MANA_INT].ToString();
-
-                                string manaCost = "";
-                                if (app.i_prof._floatStatsTable.hashTable.ContainsKey(STypeFloat.MANA_RATE_FLOAT))
-                                    manaCost = app.i_prof._floatStatsTable.hashTable[STypeFloat.MANA_RATE_FLOAT].ToString();
-
-                                //theFile.WriteLine("container guid, container wcid, container name, loot guid, loot wcid, loot name, item type, weapon type, value, material, workmanship,num tinks, gem count, gem material, spell set, spells");
-
-                                result = thisContentGUID + "," +
-                                        containerWCID + "," +
-                                        containerName + "," +
-                                        containerPos + "," +
-                                        thisContentGUID + "," +
-                                        lootWCID + "," +
-                                        lootName + "," +
-                                        itemType + "," +
-                                        weaponType + "," +
-                                        "\"" + description + "\"," +
-                                        value + "," +
-                                        materialId + "," +
-                                        workmanship + "," +
-                                        numTinks + "," +
-                                        gemCount + "," +
-                                        gemMaterial + "," +
-
-                                        clothingPriority + "," +
-                                        locations + "," +
-
-                                        wieldReq + "," +
-                                        wieldSkillType + "," +
-                                        wieldDiff + "," +
-
-                                        wieldReq2 + "," +
-                                        wieldSkillType2 + "," +
-                                        wieldDiff2 + "," +
-
-                                        itemLevel + "," +
-
-                                        spellcraft + "," +
-                                        difficulty + "," +
-                                        maxMana + "," +
-                                        manaCost + "," +
-
-                                        spellSet + "," +
-                                        spells;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return result;
-        }
-
-        private uint GetParentWeenieFromCorpse(CM_Physics.CreateObject co, Dictionary<uint, CM_Physics.CreateObject> CreateObjectList, Dictionary<uint, Position> Positions)
-        {
-
-
-
-
-            return 0;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
